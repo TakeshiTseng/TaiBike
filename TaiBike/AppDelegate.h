d903682c3162990efe854556e99af5a19ba81949
Takeshi <a86487817@gmail.com>
2014-05-27 14:31:29 +0800

    Initial Commit

---
+++ TaiBike/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TaiBike
+//
+//  Created by Takeshi on 2014/5/27.
+//  Copyright (c) 2014年 Takeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TaiBike/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TaiBike
+//
+//  Created by Takeshi on 2014/5/27.
+//  Copyright (c) 2014年 Takeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
